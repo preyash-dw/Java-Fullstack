5fe41faaeaaba750340690262e99a37d4cc8a1b1
preyash1806 <preyash1705@gmail.com>
2023-01-12 13:19:46 +0530

    java

---
+++ practice/practise2.java
@@ -0,0 +1,9 @@
+
+public class practise2{
+    public static void main(String args[]){
+        int Integer=24;
+        char String='I';
+        System.out.println(Integer);
+        System.out.println(String);
+    }
+}
