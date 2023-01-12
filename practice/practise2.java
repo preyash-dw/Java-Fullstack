f5de3248b9dba61851f7583e454e1239f9e56a46
preyash1806 <122265433+preyash1806@users.noreply.github.com>
2023-01-12 15:17:40 +0530

    Update practise2.java

--- practice/practise2.java
+++ practice/practise2.java
@@ -1,9 +1,26 @@
 
 public class practise2{
     public static void main(String args[]){
-        int Integer=24;
-        char String='I';
-        System.out.println(Integer);
-        System.out.println(String);
+       int num[]={1,2,1,1,2,3,4,4,5,5};
+       fun(num);
+    }
+    static void fun(int a[]){
+        int count=0;
+        int prev=0;
+        for(int i=0;i<a.length;i++){
+            int c=0;
+            for(int j=0;j<a.length;j++){
+                if(a[j]==a[i]){
+                    c++;
+                }
+                else{
+                    if(c>count){
+                        count=c;
+                        prev=a[i];
+                    }
+                }
+            }
+        }
+        System.out.println(prev);
     }
 }
