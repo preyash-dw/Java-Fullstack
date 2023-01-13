60f835d91a9c40bf0134962f145bdb290165b86c
preyash1806 <122265433+preyash1806@users.noreply.github.com>
2023-01-13 09:21:45 +0530

    Add files via upload

---
+++ More.java
@@ -0,0 +1,11 @@
+
+public class More {
+	int a=10;
+	String name="Hello";
+public static void main(String args[]) {
+	
+	More ob=new More();
+	System.out.println(ob.a);
+	System.out.println(ob.name);
+}
+}
